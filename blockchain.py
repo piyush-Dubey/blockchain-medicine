e4aa46d28d493906b3bd529664f18ec1fc609cb0
siddharthjain10 <34183319+siddharthjain10@users.noreply.github.com>
2017-12-02 15:46:22 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -53,3 +53,47 @@ class Blockchain():
 			self.chain = new_chain
 			return True
 		return False
+
+	def new_block(self, proof,previous_hash):
+        
+        block = {
+            'index':len(self.chain)+1,
+            'timestamp' : time(),
+            'transactions': self.current_transactions,
+            'proof':proof,
+            'previous_hash':previous_hash or self.hash(self.chain)
+            }
+        
+        self.chain.append(block)
+        return block
+
+    	def new_transaction(self,sender,recipient,amount):
+
+        	self.current_transactions.append({
+
+            	'sender'=sender,
+            	'recipient'=recipient,
+            	'amount'=amount
+            	)}
+        
+        	return self.last_block['index']+1
+
+
+    	@property
+    	def last_block(self):
+        	return self.chain[-1]
+
+
+    	@staticmethod
+    	def hash(block):        
+        	block_string=json.dumps(block,sort_keys=True).encode
+        	return hashlib.sha256(block_string).hexdigest()
+
+    	def proof_of_work(self,last_proof):
+
+        	proof=0
+
+        	while self.valid_proof(last_proof,proof) is False:
+            	proof += 1
+            
+            	return proof
