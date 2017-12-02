83c4520630a00991b35de4675a69e6b5f08fda9e
siddharthjain10 <34183319+siddharthjain10@users.noreply.github.com>
2017-12-02 19:46:21 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -151,3 +151,28 @@ class Blockchain():
         index = blockchian.new_transaction(values['sender'],values['recipient'],values['amount'])
         response = {'message': f'Transaction will be added to Block {index}'}
         return jsonify(response), 201
+
+			
+    @app.route('/chain', methods=['GET'])
+    def full_chain():
+        response={
+            'chain':blockchain.chain,
+            'length': len(blockchain.chain),
+        }
+        return jsonify(response), 200
+
+                                        
+    @app.route('/nodes/register',methods=['POST'])
+    def register_nodes():
+        values=request.get_json()
+
+        nodes=values.get('nodes')
+        if nodes is None:
+            return 'Error: Please supply valid list of nodes', 400
+        for node in nodes:
+            blockchain.register_node(node)
+        response={
+            'message':'New nodes have been added',
+            'total_nodes': list(blockchain.nodes),
+        }
+        return jsonify(response),201		
