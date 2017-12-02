724dd3466dced3470dda837d6428c608ff106009
piyush-Dubey <piyushdubey1996@gmail.com>
2017-12-03 01:10:12 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -57,15 +57,15 @@ class Blockchain():
 		return False
 
 	def new_block(self, proof,previous_hash):
-        
+		
 		block = {
 			'index':len(self.chain)+1,
 			'timestamp' : time(),
 			'transactions': self.current_transactions,
 			'proof':proof,
-			'previous_hash':previous_hash or self.hash(self.chain),
+			'previous_hash':previous_hash or self.hash(self.chain[-1]),
 		}
-
+		self.current_transactions = []
 		self.chain.append(block)
 		return block
 
