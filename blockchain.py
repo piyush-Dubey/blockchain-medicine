8b53b97f329d0cd288babec7f016bbb765b5c6bb
piyush-Dubey <piyushdubey1996@gmail.com>
2017-12-03 00:49:21 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -71,9 +71,9 @@ class Blockchain():
 
 	def new_transaction(self,sender,recipient,amount):
 		self.current_transactions.append({
-		'sender'=sender,
-		'recipient'=recipient,
-		'amount'=amount,
+			'sender'=sender,
+			'recipient'=recipient,
+			'amount'=amount,
 		})
 		
 		return self.last_block['index']+1
